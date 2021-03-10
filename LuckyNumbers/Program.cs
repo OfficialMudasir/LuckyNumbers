02bb7ec3faa07eadb966b7a1b17abc3c83dfe26f
wani <itzahmad395@gmail.com>
2021-03-10 11:40:57 +0530

    Add project files.

---
+++ LuckyNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9, y = 1;
+            Lucky_8(x, y);
+
+        }
+        public static bool Lucky_8(int a,int b)
+        {
+            bool result = false;
+            
+            int sum = a + b;
+            int sub = a - b;
+            if (sum == 8 || sub == 8 || a == 8 || b == 8)
+            {
+                result = true;
+                Console.WriteLine("Input: a = " + a + ", b = " + b);
+                Console.WriteLine("Output: " + result);
+                return result;
+
+            }
+            else
+            {
+                Console.WriteLine("Input: a = " + a + ", b = " + b);
+                Console.WriteLine("Output: " + result);
+                return result;
+            }
+        }
+    }
+}
